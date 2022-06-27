9fe1f9ba7139ba546adaf7d4d25d931c73238f30
phaydara <phaydara@gmail.com>
2022-06-27 13:21:29 -0500

    added comments in starter code for clarity

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+
+// NOTES: HelloWorld component is imported for App to use
 import HelloWorld from './components/HelloWorld.vue'
 
+// NOTES: App and components are exported for main.js to create Vue app
 export default {
   name: 'App',
   components: {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+// NOTES: this is where the Vue app is created
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// NOTES: App instance won't render until its .mount() method is called
 createApp(App).mount('#app')
