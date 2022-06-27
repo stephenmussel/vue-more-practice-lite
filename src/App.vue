8de7d5d1dcd3043c6ad8d8e30dc9cff363a43a95
phaydara <phaydara@gmail.com>
2022-06-27 13:40:45 -0500

    displayed NumCounter

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NumCounter />
 </template>
 
 <script>
 
 // NOTES: HelloWorld component is imported for App to use
 import HelloWorld from './components/HelloWorld.vue'
+import NumCounter from './components/NumCounter/NumCounter.vue'
 
 // NOTES: App and components are exported for main.js to create Vue app
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NumCounter
   }
 }
 </script>
---
+++ src/components/NumCounter/NumCounter.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Counter: {{ count }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'NumCounter',
+        data() {
+            return {
+                count: 0
+            }
+        }
+    }
+</script>
